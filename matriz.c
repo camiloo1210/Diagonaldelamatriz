e26bb700e889f72ca10786845993b76ebe64d76b
camiloo1210 <130024293+camiloo1210@users.noreply.github.com>
2023-05-17 15:34:02 -0500

    2 commit
    (inicialización puntero-matriz llena de 0)

--- matriz.c
+++ matriz.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    int f,c;
+    int f, c;
+    int **puntero = calloc(f, sizeof(int *));
     printf("Ingrese el numero de filas");
-    scanf("%i",&f);
+    scanf("%i", &f);
     printf("Ingrese el numero de columnas");
-    scanf("%i",&c);
+    scanf("%i", &c);
+    for (int i = 0; i < f; i++)
+    {
+        puntero[i] = calloc(c, sizeof(int));
+    }
+
+
 }
